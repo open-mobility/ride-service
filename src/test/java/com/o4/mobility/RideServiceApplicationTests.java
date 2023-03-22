67a635d12872274cc2b6c38307541ba14e4c2d28
Mazhar Hassan <netsurgeon@gmail.com>
2023-03-22 13:44:59 +0500

    Initialize project

---
+++ src/test/java/com/o4/mobility/RideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o4.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
